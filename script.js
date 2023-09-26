b198648bc55fb8de9131f9ac749d183365bf1b18
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-09-26 13:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+const scrollContainer = document.getElementById("infi-list");
 
+let i=0;
+function addItems(){
+	let item = document.createElement("li");
+	item.innerText = `Item ${++i}`;
+	scrollContainer.appendChild(item);
+}
+
+for (let i=0; i<10; i++){
+	addItems();
+}
+
+scrollContainer.addEventListener("scroll", ()=> {
+	const scrollableHeight = scrollContainer.scrollHeight - scrollContainer.clientHeight;
+	const remainingScrollTop = scrollContainer.scrollTop;
+
+	let threshold = Math.abs(scrollableHeight - remainingScrollTop);
+	if (threshold <= 5){
+		for (let i=0; i<2; i++){
+			addItems();
+		}
+	}
+});
